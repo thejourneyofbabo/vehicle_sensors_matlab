38fe741aae4288dfc1975373a1eff5c7144b3972
Jisang Yun <journeyofbabo@gmail.com>
2025-03-20 12:30:51 +0900

    .

--- 03_Noise Filtering/estimator/PulseCountingMethod.m
+++
@@ -1,9 +0,0 @@
-function [estimated_speed] = PulseCountingMethod(t,pulses, pulsesPerRevolution, time_interval)
-    estimated_speed = zeros([length(t) 1]);
-    
-    %% ToDo
-    for idx = 1:length(t)
-        estimated_speed(idx) = 0;
-    end
-end
-
