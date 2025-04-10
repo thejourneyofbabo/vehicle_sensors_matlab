c58bab5ad61ab6ea7c62b5fa43cd269cba76f4ed
Jisang Yun <journeyofbabo@gmail.com>
2025-04-10 16:02:57 +0900

    gnns update

--- 06_GNSS Practice/wgs84_conversion/EnuToWgs84.m
+++ 06_GNSS Practice/wgs84_conversion/EnuToWgs84.m
@@ -1,12 +1,28 @@
 function llh = EnuToWgs84(enu,ref_llh)
 llh = zeros(size(enu));
+ELLIPSE_A = 6378137;
+ELLIPSE_B = 6356752.314;
+
+ref_latt = ref_llh(1);
+ref_logt = ref_llh(2);
+ref_hight = ref_llh(3);
+
+M = ((ELLIPSE_A * ELLIPSE_B)^2) / ((ELLIPSE_A * cosd(ref_latt))^2 + (ELLIPSE_B * sind(ref_latt))^2)^(3/2);
+N = (ELLIPSE_A^2) / sqrt((ELLIPSE_A * cosd(ref_latt))^2 + (ELLIPSE_B * sind(ref_latt))^2);
 
 %% Todo
 
 for idx = 1 : size(llh,1)
-    llh(idx, 1) = 0.0;
-    llh(idx, 2) = 0.0;
-    llh(idx, 3) = 0.0;
+    d_latt = enu(idx, 2) / (M + ref_hight);
+    d_logt = enu(idx, 1) / ((N + ref_hight) * cosd(ref_latt));
+
+    latt = d_latt * (180 / pi) + ref_latt ;
+    logt = d_logt * (180 / pi) + ref_logt ;
+    h = enu(idx, 3) + ref_hight;
+
+    llh(idx, 1) = latt;
+    llh(idx, 2) = logt;
+    llh(idx, 3) = h;
 end
 
 end
--- 06_GNSS Practice/wgs84_conversion/Wgs84ToEnu.m
+++ 06_GNSS Practice/wgs84_conversion/Wgs84ToEnu.m
@@ -1,12 +1,30 @@
 function enu = Wgs84ToEnu(llh,ref_llh)
 enu = zeros(size(llh));
+ELLIPSE_A = 6378137;
+ELLIPSE_B = 6356752.314;
+% ELLIPSE_F = 298.2572236;
+
+ref_latt = ref_llh(1);
+ref_logt = ref_llh(2);
+ref_hight = ref_llh(3);
+
+M = ((ELLIPSE_A * ELLIPSE_B)^2) / ((ELLIPSE_A * cosd(ref_latt))^2 + (ELLIPSE_B * sind(ref_latt))^2)^(3/2);
+N = (ELLIPSE_A^2) / sqrt((ELLIPSE_A * cosd(ref_latt))^2 + (ELLIPSE_B * sind(ref_latt))^2);
 
 %% Todo
 
 for idx = 1 : size(llh,1)
-    enu(idx, 1) = 0.0;
-    enu(idx, 2) = 0.0;
-    enu(idx, 3) = 0.0;
+    h = llh(idx, 3);
+    d_latt = (llh(idx, 1) - ref_latt) * pi /180;
+    d_logt = (llh(idx, 2) - ref_logt) * pi /180;
+    d_h = h -ref_hight;
+
+    d_N = (M + ref_hight) * d_latt;
+    d_E = (N + ref_hight) * cosd(ref_latt) * d_logt;
+
+    enu(idx, 1) = d_E;
+    enu(idx, 2) = d_N;
+    enu(idx, 3) = d_h;
 end
 
 end
